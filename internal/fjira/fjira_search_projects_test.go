44e21ce470d39b4b4ca591394eb030efc15370b2
mk-5 <mateusz@mk5.pl>
2022-06-13 23:35:56 +0200

    Add units for search projects view

---
+++ internal/fjira/fjira_search_projects_test.go
@@ -0,0 +1,62 @@
+package fjira
+
+import (
+	"bytes"
+	"github.com/gdamore/tcell/v2"
+	"github.com/mk5/fjira/internal/app"
+	"github.com/mk5/fjira/internal/jira"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestNewProjectsSearchView(t *testing.T) {
+	screen := tcell.NewSimulationScreen("utf-8")
+	screen.Init() //nolint:errcheck
+	defer screen.Fini()
+	tests := []struct {
+		name string
+	}{
+		{"should initialize & draw projects search view"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// given
+			app.CreateNewAppWithScreen(screen)
+			CreateNewFjira(&fjiraSettings{})
+			api := jira.NewJiraApiMock(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(200)
+				_, _ = w.Write([]byte(`[{"id": "1", "name": "Test", "key": "TEST"}, {"id": "2", "name": "Fjira", "key":"FJIR"}]`))
+			})
+			_ = SetApi(api)
+			view := NewProjectsSearchView()
+
+			// when
+			view.Init()
+			<-time.After(1 * time.Second)
+			query := "FJIR"
+			for _, key := range query {
+				view.HandleKeyEvent(tcell.NewEventKey(-1, key, tcell.ModNone))
+			}
+			view.Update()
+			view.Resize(screen.Size())
+			<-time.After(1 * time.Second)
+			view.Update()
+			view.Draw(screen)
+			<-time.After(1 * time.Second)
+
+			var buffer bytes.Buffer
+			contents, x, y := screen.(tcell.SimulationScreen).GetContents()
+			screen.Show()
+			for i := 0; i < x*y; i++ {
+				buffer.Write(contents[i].Bytes)
+			}
+			result := buffer.String()
+
+			// then
+			assert.Contains(t, result, "Fjira")
+			assert.NotContains(t, result, "TEST")
+		})
+	}
+}
