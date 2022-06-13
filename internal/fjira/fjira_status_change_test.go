307f7eb0dfc039a8f0ea8503e321cf1056a0b717
mk-5 <mateusz@mk5.pl>
2022-06-13 23:30:56 +0200

    Add units for status_change and assign_user views

---
+++ internal/fjira/fjira_status_change_test.go
@@ -0,0 +1,122 @@
+package fjira
+
+import (
+	"bytes"
+	"github.com/gdamore/tcell/v2"
+	"github.com/mk5/fjira/internal/app"
+	"github.com/mk5/fjira/internal/jira"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestNewStatusChangeView(t *testing.T) {
+	screen := tcell.NewSimulationScreen("utf-8")
+	screen.Init() //nolint:errcheck
+	defer screen.Fini()
+
+	type args struct {
+		issue *jira.JiraIssue
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"should initialize & draw status change view", args{issue: &jira.JiraIssue{}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// given
+			app.CreateNewAppWithScreen(screen)
+			CreateNewFjira(&fjiraSettings{})
+			api := jira.NewJiraApiMock(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(200)
+				_, _ = w.Write([]byte(`{
+    "transitions": [
+        {
+            "id": "11",
+            "name": "To Do"
+        },
+        {
+            "id": "21",
+            "name": "In Progress"
+        }
+    ]
+}`)) //nolint:errcheck
+			})
+			_ = SetApi(api)
+			view := NewStatusChangeView(tt.args.issue)
+
+			// when
+			view.Init()
+			<-time.After(1 * time.Second)
+			query := "in progress"
+			for _, key := range query {
+				view.HandleKeyEvent(tcell.NewEventKey(-1, key, tcell.ModNone))
+			}
+			view.Update()
+			view.Resize(screen.Size())
+			<-time.After(1 * time.Second)
+			view.Update()
+			view.Draw(screen)
+			<-time.After(1 * time.Second)
+
+			var buffer bytes.Buffer
+			contents, x, y := screen.(tcell.SimulationScreen).GetContents()
+			screen.Show()
+			for i := 0; i < x*y; i++ {
+				buffer.Write(contents[i].Bytes)
+			}
+			result := buffer.String()
+
+			// then
+			assert.Contains(t, result, "In Progress")
+			assert.NotContains(t, result, "To Do")
+		})
+	}
+}
+
+func Test_fjiraStatusChangeView_changeStatusForTicket(t *testing.T) {
+	screen := tcell.NewSimulationScreen("utf-8")
+	screen.Init() //nolint:errcheck
+	defer screen.Fini()
+
+	type args struct {
+		issue  *jira.JiraIssue
+		status *jira.JiraIssueTransition
+	}
+
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"should send change status request", args{issue: &jira.JiraIssue{Key: "ABC", Id: "123"}, status: &jira.JiraIssueTransition{Name: "In Progress", Id: "333"}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// given
+			app.CreateNewAppWithScreen(screen)
+			CreateNewFjira(&fjiraSettings{})
+			view := NewStatusChangeView(tt.args.issue)
+
+			// when
+			changeStatusRequestSent := make(chan bool)
+			_ = SetApi(jira.NewJiraApiMock(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(200)
+				_, _ = w.Write([]byte(`{}`))
+
+				assert.Contains(t, r.RequestURI, tt.args.issue.Key)
+				changeStatusRequestSent <- true
+			}))
+			go view.changeStatusForTicket(tt.args.issue, tt.args.status)
+
+			// then
+			select {
+			case <-changeStatusRequestSent:
+			case <-time.After(3 * time.Second):
+				t.Fail()
+			}
+		})
+	}
+}
---
+++ internal/fjira/fjira_user_assign_test.go
@@ -0,0 +1,112 @@
+package fjira
+
+import (
+	"bytes"
+	"github.com/gdamore/tcell/v2"
+	"github.com/mk5/fjira/internal/app"
+	"github.com/mk5/fjira/internal/jira"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestNewAssignChangeView(t *testing.T) {
+	screen := tcell.NewSimulationScreen("utf-8")
+	screen.Init() //nolint:errcheck
+	defer screen.Fini()
+
+	type args struct {
+		issue *jira.JiraIssue
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"should initialize & draw assign user view", args{issue: &jira.JiraIssue{}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// given
+			app.CreateNewAppWithScreen(screen)
+			CreateNewFjira(&fjiraSettings{})
+			api := jira.NewJiraApiMock(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(200)
+				_, err := w.Write([]byte(`[{"id": "U1", "displayName": "Bob"}, {"id": "U2", "displayName": "John"}]`))
+				println(err)
+			})
+			_ = SetApi(api)
+			view := NewAssignChangeView(tt.args.issue)
+
+			// when
+			view.Init()
+			<-time.After(1 * time.Second)
+			query := "bob"
+			for _, key := range query {
+				view.HandleKeyEvent(tcell.NewEventKey(-1, key, tcell.ModNone))
+			}
+			view.Update()
+			view.Resize(screen.Size())
+			<-time.After(1 * time.Second)
+			view.Update()
+			view.Draw(screen)
+			<-time.After(1 * time.Second)
+
+			var buffer bytes.Buffer
+			contents, x, y := screen.(tcell.SimulationScreen).GetContents()
+			screen.Show()
+			for i := 0; i < x*y; i++ {
+				buffer.Write(contents[i].Bytes)
+			}
+			result := buffer.String()
+
+			// then
+			assert.Contains(t, result, "Bob")
+			assert.NotContains(t, result, "John")
+		})
+	}
+}
+
+func Test_fjiraAssignChangeView_doAssignmentChange(t *testing.T) {
+	screen := tcell.NewSimulationScreen("utf-8")
+	screen.Init() //nolint:errcheck
+	defer screen.Fini()
+
+	type args struct {
+		issue *jira.JiraIssue
+		user  *jira.JiraUser
+	}
+
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"should send assign user request", args{issue: &jira.JiraIssue{Key: "ABC", Id: "123"}, user: &jira.JiraUser{DisplayName: "Bob", AccountId: "333"}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// given
+			app.CreateNewAppWithScreen(screen)
+			CreateNewFjira(&fjiraSettings{})
+			view := NewAssignChangeView(tt.args.issue)
+
+			// when
+			changeStatusRequestSent := make(chan bool)
+			_ = SetApi(jira.NewJiraApiMock(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(200)
+				_, _ = w.Write([]byte(`{}`))
+
+				assert.Contains(t, r.RequestURI, tt.args.issue.Key)
+				changeStatusRequestSent <- true
+			}))
+			go view.doAssignmentChange(tt.args.issue, tt.args.user)
+
+			// then
+			select {
+			case <-changeStatusRequestSent:
+			case <-time.After(3 * time.Second):
+				t.Fail()
+			}
+		})
+	}
+}
