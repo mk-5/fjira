41557bf66f92036d52430a68903cc70c7a6757ef
mk-5 <mateusz@mk5.pl>
2022-11-26 19:02:18 +0100

    refactor: test for open_link.go

--- internal/app/open_link.go
+++ internal/app/open_link.go
@@ -8,18 +8,21 @@ import (
 )
 
 func OpenLink(url string) {
-	var err error
-	switch runtime.GOOS {
-	case "linux":
-		err = exec.Command("xdg-open", url).Start()
-	case "windows":
-		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-	case "darwin":
-		err = exec.Command("open", url).Start()
-	default:
-		err = fmt.Errorf("unsupported platform")
-	}
+	err := openLinkForSystem(runtime.GOOS, url)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func openLinkForSystem(system string, url string) error {
+	switch system {
+	case "linux":
+		return exec.Command("xdg-open", url).Start()
+	case "windows":
+		return exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		return exec.Command("open", url).Start()
+	default:
+		return fmt.Errorf("unsupported platform")
 	}
 }
--- internal/app/open_link_test.go
+++ internal/app/open_link_test.go
@@ -14,3 +14,23 @@ func TestOpenLink(t *testing.T) {
 		})
 	}
 }
+
+func TestOpenLink_forSystem(t *testing.T) {
+	type args struct {
+		system string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"should run OpenLink func for system", args{system: "windows"}},
+		{"should run OpenLink func for system", args{system: "linux"}},
+		{"should run OpenLink func for system", args{system: "darwin"}},
+		{"should run OpenLink func for system", args{system: "unknown"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_ = openLinkForSystem(tt.args.system, "/dev/null")
+		})
+	}
+}
