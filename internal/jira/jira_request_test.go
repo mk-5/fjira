7cc96e6819d80001e77d3dfe28addce4b8d36343
mk-5 <mateusz@mk5.pl>
2024-11-12 23:05:04 +0100

    misc(#122): add unit test for jira request URI combine logic

--- internal/jira/jira_request_test.go
+++ internal/jira/jira_request_test.go
@@ -38,3 +38,26 @@ func Test_httpApi_jiraRequest_should_return_error_when_invalid_params(t *testing
 	// then
 	assert.NotNil(t, err)
 }
+
+func Test_jiraRequest_combinePaths(t *testing.T) {
+	tests := []struct {
+		name     string
+		apiUrl   string
+		restPath string
+		wanted   string
+	}{
+		{"should add label without error", "http://localhost", "/api1", "http://localhost/api1"},
+		{"should add label without error", "http://localhost/", "/api1", "http://localhost/api1"},
+		{"should add label without error", "http://localhost/jira-api-v2", "/api1", "http://localhost/jira-api-v2/api1"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u, _ := url.Parse(tt.apiUrl)
+			api := &httpApi{restUrl: u}
+
+			result, _ := api.jiraRequestUrl(tt.restPath, nil)
+
+			assert.Equal(t, tt.wanted, result)
+		})
+	}
+}
